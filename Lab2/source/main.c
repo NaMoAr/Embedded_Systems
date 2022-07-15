41f9aa6ca25fae0550e8adf29ea6fe93178b7048
NaMoAr <59351131+NaMoAr@users.noreply.github.com>
2022-07-15 10:43:48 -0700

    Update main.c

--- Lab2/source/main.c
+++ Lab2/source/main.c
@@ -1,7 +1,7 @@
-/*	Author: lab
+/*	Author: Najmeh
  *  Partner(s) Name: NONE
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab #2  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
